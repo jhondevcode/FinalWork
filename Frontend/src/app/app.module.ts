2307b837cd0bfac6dbe9edfacf9a3df97f1c660a
jhondevcode <jhondev.code@gmail.com>
2022-01-03 18:44:55 -0500

    The app module was updated

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ComponentsModule } from './components/components.module';
+import { SharedModule } from './shared/shared.module';
+import { DataModule } from './data/data.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -10,9 +16,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    SharedModule,
+    DataModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-PE'
+    }
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
