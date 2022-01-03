a892573210093cfa487d02871fba7002eaf5efa5
jhondevcode <jhondev.code@gmail.com>
2022-01-03 17:29:20 -0500

    Backed has been created

---
+++ Backend/WorkRest/WorkRest/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WorkRest project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WorkRest.settings')
+
+application = get_wsgi_application()
