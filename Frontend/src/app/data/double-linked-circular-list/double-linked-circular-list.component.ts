f86e850d5daa03ae6da7a9aaf8940b2ccec4533d
jhondevcode <jhondev.code@gmail.com>
2022-01-03 19:03:56 -0500

    DoubleLinkedCircularList component added

---
+++ Frontend/src/app/data/double-linked-circular-list/double-linked-circular-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-double-linked-circular-list',
+  templateUrl: './double-linked-circular-list.component.html',
+  styleUrls: ['./double-linked-circular-list.component.scss']
+})
+export class DoubleLinkedCircularListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
