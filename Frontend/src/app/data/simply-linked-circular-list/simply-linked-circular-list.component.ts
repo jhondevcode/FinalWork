e32de2a59e4193c4304e814d00b24f210e020ad1
jhondevcode <jhondev.code@gmail.com>
2022-01-03 19:03:04 -0500

    SimplyLinkedCircularList component added

---
+++ Frontend/src/app/data/simply-linked-circular-list/simply-linked-circular-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simply-linked-circular-list',
+  templateUrl: './simply-linked-circular-list.component.html',
+  styleUrls: ['./simply-linked-circular-list.component.scss']
+})
+export class SimplyLinkedCircularListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
