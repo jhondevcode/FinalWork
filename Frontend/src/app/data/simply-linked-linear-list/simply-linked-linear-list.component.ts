f8b32772c375dfacfb545303a82c57a03454bfb0
jhondevcode <jhondev.code@gmail.com>
2022-01-03 19:00:24 -0500

    SimplyLinkedLinearList component added

---
+++ Frontend/src/app/data/simply-linked-linear-list/simply-linked-linear-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simply-linked-linear-list',
+  templateUrl: './simply-linked-linear-list.component.html',
+  styleUrls: ['./simply-linked-linear-list.component.scss']
+})
+export class SimplyLinkedLinearListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
