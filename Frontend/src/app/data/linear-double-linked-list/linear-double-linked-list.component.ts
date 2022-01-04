efdf48868fb1e474aae9442fc7f39b7f41318654
jhondevcode <jhondev.code@gmail.com>
2022-01-03 19:03:37 -0500

    LinearDoubleLinkedList component added

---
+++ Frontend/src/app/data/linear-double-linked-list/linear-double-linked-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linear-double-linked-list',
+  templateUrl: './linear-double-linked-list.component.html',
+  styleUrls: ['./linear-double-linked-list.component.scss']
+})
+export class LinearDoubleLinkedListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
