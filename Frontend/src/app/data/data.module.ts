423c19051d21e8f4b4aeb5d2c0c731ce404b8c5d
jhondevcode <jhondev.code@gmail.com>
2022-01-03 19:02:09 -0500

    Data module was updated

--- Frontend/src/app/data/data.module.ts
+++ Frontend/src/app/data/data.module.ts
@@ -1,40 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LinearDoubleLinkedListComponent } from './linear-double-linked-list/linear-double-linked-list.component';
-import { LinkedSimpleLinearListComponent } from './linked-simple-linear-list/linked-simple-linear-list.component';
-import { SimpleCircularLinkedListComponent } from './simple-circular-linked-list/simple-circular-linked-list.component';
 import { DoubleLinkedCircularListComponent } from './double-linked-circular-list/double-linked-circular-list.component';
 import { QueueComponent } from './queue/queue.component';
 import { StackComponent } from './stack/stack.component';
 import { BinaryTreeComponent } from './binary-tree/binary-tree.component';
 import { GraphsComponent } from './graphs/graphs.component';
 import { DatabaseComponent } from './database/database.component';
+import { SimplyLinkedLinearListComponent } from './simply-linked-linear-list/simply-linked-linear-list.component';
+import { SimplyLinkedCircularListComponent } from './simply-linked-circular-list/simply-linked-circular-list.component';
 
 @NgModule({
   declarations: [
     LinearDoubleLinkedListComponent,
-    LinkedSimpleLinearListComponent,
-    SimpleCircularLinkedListComponent,
     DoubleLinkedCircularListComponent,
     QueueComponent,
     StackComponent,
     BinaryTreeComponent,
     GraphsComponent,
-    DatabaseComponent
+    DatabaseComponent,
+    SimplyLinkedLinearListComponent,
+    SimplyLinkedCircularListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LinearDoubleLinkedListComponent,
-    LinkedSimpleLinearListComponent,
-    SimpleCircularLinkedListComponent,
     DoubleLinkedCircularListComponent,
     QueueComponent,
     StackComponent,
     BinaryTreeComponent,
     GraphsComponent,
-    DatabaseComponent
+    DatabaseComponent,
+    SimplyLinkedLinearListComponent,
+    SimplyLinkedCircularListComponent
   ]
 })
 
